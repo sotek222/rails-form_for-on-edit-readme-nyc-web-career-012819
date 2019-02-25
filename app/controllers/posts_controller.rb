31746e70f14663ebf6c16dd33e735187a1ba92ee
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-24 22:38:11 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
+	  @post = Post.new(params.require(:post))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
@@ -25,7 +23,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(params.require(:post))
 	  redirect_to post_path(@post)
 	end
 end
